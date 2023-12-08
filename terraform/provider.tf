707505ed33343e39ff01256967793453a0995d09
loveth oballe <insiible@gmail.com>
2023-12-08 07:58:11 -0500

    Adding terraform config files

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      # version = "5.30.0"
-    }
-  }
+  # required_providers {
+  #   aws = {
+  #     source = "hashicorp/aws"
+  #     # version = "5.30.0"
+  #   }
+  # }
   backend "s3" {
     bucket = "backend-bk"
     key    = "backend-bk/bsk-stfile"
